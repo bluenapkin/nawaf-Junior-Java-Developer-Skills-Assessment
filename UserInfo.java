caf7cacd60b198519746cf270db636d762800723
Your Name <you@example.com>
2025-07-22 09:46:07 +0400

    i add Create constructor that takes all parameters

--- UserInfo.java
+++ UserInfo.java
@@ -2,6 +2,13 @@ public class UserInfo {
     String name = "nawaff";
     int age = 55;
     String email = "nawaffnawaff123@gmail.com";
-    boolean isAcyive = true;
+    boolean isActive = true;
+
+    public UserInfo(String name, int age , String email , boolean isActive){
+        this.name=  name;
+        this.age  = age;
+        this.email = email;
+        this.isActive = isActive;
+    }
 
 }
